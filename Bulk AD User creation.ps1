2e5007905b57607b9a5e932f6e98c3c595033eee
Kyle Elliott <kelliott@ciotech.us>
2018-07-10 15:21:44 -0400

    Help block addition Parameter addition to remove static pathing

--- Bulk AD User creation.ps1
+++ Bulk AD User creation.ps1
@@ -1,14 +1,35 @@
+<#Windows PowerShell Code
+.description
+  BulkAdUserCreation.ps1
+.Synopsis
+  Creates a batch of AD users based on a CSV Provided.
+.Parameter CSV
+  Pass a CSV file with the following headers
+  username,password,Firstname,Lastname,EmailAddress,OU,UPN
+.Example
+  BulkAdUserCreation.ps1 -csv c:\temp\users.csv
+.NOTES
+  By Kyle Elliott
+  kelliott(at)ciotech(dot)us
+  Provided as is, without warranty
+  #>
 ﻿# Import active directory module for running AD cmdlets
-Import-Module activedirectory
-  
-#Store the data from ADUsers.csv in the $ADUsers variable
-$ADUsers = Import-csv .\ADUsers.csv
+Param(
+  [Parameter(Mandatory=$True)]
+  [string]$csv
+)
 
-#Loop through each row containing user details in the CSV file 
+
+Import-Module activedirectory
+
+#Store the data from ADUsers.csv in the $ADUsers variable
+$ADUsers = Import-csv $csv
+
+#Loop through each row containing user details in the CSV file
 foreach ($User in $ADUsers)
 {
 	#Read user data from each field in each row and assign the data to a variable as below
-		
+
 	$Username 	= $User.username
 	$Password 	= $User.Password
 	$Firstname 	= $User.FirstName
@@ -26,7 +47,7 @@ foreach ($User in $ADUsers)
 	else
 	{
 		#User does not exist then proceed to create the new user account
-		
+
         #Account will be created in the OU provided by the $OU variable read from the CSV file
 		New-ADUser `
             -SamAccountName $Username `
