32005d91a0164a875a765882a415219da90a0113
Kyle Elliott <kelliott@ciotech.us>
2018-07-10 15:28:45 -0400

    Cleaned up wild tabs Adjusted comments to make more clear

--- Bulk AD User creation.ps1
+++ Bulk AD User creation.ps1
@@ -6,6 +6,8 @@
 .Parameter CSV
   Pass a CSV file with the following headers
   username,password,Firstname,Lastname,EmailAddress,OU,UPN
+  OU should be the full DN (IE: "OU=sales,OU=corp,DC=Consoto,DC=com")
+  Username will be sAMAccountName
 .Example
   BulkAdUserCreation.ps1 -csv c:\temp\users.csv
 .NOTES
@@ -13,13 +15,15 @@
   kelliott(at)ciotech(dot)us
   Provided as is, without warranty
   #>
+
 ﻿# Import active directory module for running AD cmdlets
+
 Param(
   [Parameter(Mandatory=$True)]
   [string]$csv
 )
 
-
+#this is an active directory script
 Import-Module activedirectory
 
 #Store the data from ADUsers.csv in the $ADUsers variable
@@ -28,27 +32,27 @@ $ADUsers = Import-csv $csv
 #Loop through each row containing user details in the CSV file
 foreach ($User in $ADUsers)
 {
-	#Read user data from each field in each row and assign the data to a variable as below
+#Read user data from each field in each row and assign the data to a variable as below
 
 	$Username 	= $User.username
 	$Password 	= $User.Password
 	$Firstname 	= $User.FirstName
 	$Lastname 	= $User.LastName
-    $Email      = $User.EmailAddress
-	$OU 		= $User.OU #This field refers to the OU the user account is to be created in
-    $UPN        = $User.UPN
+  $Email      = $User.EmailAddress
+	$OU 		    = $User.OU #This field refers to the OU the user account is to be created in
+  $UPN        = $User.UPN
 
-	#Check to see if the user already exists in AD
+#Check to see if the user already exists in AD
 	if (Get-ADUser -F {SamAccountName -eq $Username})
 	{
-		 #If user does exist, give a warning
+#If user does exist, give a warning
 		 Write-Warning "A user account with username $Username already exist in Active Directory."
 	}
 	else
 	{
-		#User does not exist then proceed to create the new user account
+#User does not exist then proceed to create the new user account
 
-        #Account will be created in the OU provided by the $OU variable read from the CSV file
+#Account will be created in the OU provided by the $OU variable read from the CSV file
 		New-ADUser `
             -SamAccountName $Username `
             -UserPrincipalName $UPN `
